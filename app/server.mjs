e1550b5226b996e1783d66b91bdeb387c662fd4e
cjharcum <Cjharcum@ualr.edu>
2022-02-18 19:46:30 -0600

    l

--- app/server.mjs
+++ app/server.mjs
@@ -20,7 +20,12 @@ const server = http.createServer((req, res) =>
 }
   server.listen(8080)
 });
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
 app.use(express.static(__dirname + '/website'));
 app.listen(process.env.PORT || 8080);
  
